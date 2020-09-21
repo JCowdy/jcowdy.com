3f61fc8e2bf0fcf2b8c8091556d16dad99ba1b3a
JCowdy <Jason.Cowdy@gmail.com>
2020-09-20 20:29:43 -0500

    add 404 page

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Home } from './pages'
+import { Home, NotFound } from './pages'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
