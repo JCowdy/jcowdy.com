eb7524b883de3f4949fd0b59e00a4fdbe7a61843
JCowdy <Jason.Cowdy@gmail.com>
2020-09-25 09:43:03 -0500

    add initial tests

---
+++ client/pages/home/Home.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Home from './Home'
+import Title from './Title'
+import Links from './Links'
+
+describe('Links', () => {
+
+  let cmp
+
+  beforeAll(() => {
+    cmp = shallow(<Home />)
+  })
+
+  it('Renders', () => {
+    expect(cmp).not.toBeUndefined()
+  })
+
+  it('Contains the Title component', () => {
+    expect(cmp.find(Title)).toHaveLength(1)
+  })
+
+  it('Contains the Links component', () => {
+    expect(cmp.find(Links)).toHaveLength(1)
+  })
+})
---
+++ client/pages/home/Title.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Title from './Title'
+
+describe('Title', () => {
+
+  let cmp
+
+  beforeAll(() => {
+    cmp = shallow(<Title />)
+  })
+
+  it('Renders', () => {
+    expect(cmp).not.toBeUndefined()
+  })
+
+  it('Displays the title text', () => {
+    expect(cmp.text()).toMatch(/Hello World/)
+  })
+})
