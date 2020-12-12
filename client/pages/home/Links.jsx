55784b319efd96dbd364ed9fde5708f0fc9c5648
JCowdy <Jason.Cowdy@gmail.com>
2020-12-12 08:23:10 -0600

    add accessible labels to icon links

--- client/pages/home/Links.jsx
+++ client/pages/home/Links.jsx
@@ -3,9 +3,9 @@ import Link from '../../components/Link'
 
 const Links = () => (
   <div>
-    <Link href='https://www.linkedin.com/in/jasoncowdy' icon='linkedin' />
-    <Link href='https://github.com/jcowdy' icon='github' />
-    <Link href='https://twitter.com/jasoncowdy' icon='twitter' />
+    <Link href='https://www.linkedin.com/in/jasoncowdy' icon='linkedin' label='Linkedin Profile' />
+    <Link href='https://github.com/jcowdy' icon='github' label='Github Profile' />
+    <Link href='https://twitter.com/jasoncowdy' icon='twitter' label='Twitter Profile'/>
   </div>
 )
 
