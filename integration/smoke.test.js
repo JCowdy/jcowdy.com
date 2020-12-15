ded40d123c1f996e388d9edde74ead32c3e96cc4
jcowdy <jason.cowdy@gmail.com>
2020-12-14 20:37:01 -0600

    Revert "add jest-puppeteer"
    This reverts commit 1db49d6440d8bbfcf7f060e839b37854cb6f786a.

--- integration/smoke.test.js
+++
@@ -1,9 +0,0 @@
-describe('Smoke Test', () => {
-  beforeAll(async () => {
-    await page.goto('https://jcowdy.com')
-  })
-
-  it('should be titled "appropriately"', async () => {
-    await expect(page.title()).resolves.toMatch('Jason Cowdy')
-  })
-})
